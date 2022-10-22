1ed5fa12f8331316f8433af8e42c13fa39f1f20e
Yumi Nishiguchi <yuuamaneteste@gmail.com>
2022-10-21 21:37:22 -0300

    Update PW_BD.sql

--- PW_BD.sql
+++ PW_BD.sql
@@ -24,7 +24,7 @@ VALUES('Yumi',1);
 INSERT INTO TB_ALUNOS (Nome, CursoId)
 VALUES('Shibata',1);
 INSERT INTO TB_ALUNOS (Nome, CursoId)
-VALUES('Guilherme',1);(Nome, CursoId)
+VALUES('Guilherme',1);
 INSERT INTO TB_ALUNOS (Nome, CursoId)
 VALUES('Debora',1);
 VALUES('Miguel',1);(Nome, CursoId)
